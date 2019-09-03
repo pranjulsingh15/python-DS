ed685fe19f441ebcd8c08dae5427964e2c8aac6c
pranjulsingh15 <pranjulsingh15@gmail.com>
2019-09-03 10:19:22 +0530

    tree optimized added more functions

--- mycode/tree.py
+++ mycode/tree.py
@@ -1,85 +1,108 @@
-#code for tree
+# code for tree
 import queue
-class node:
-    def __init__(self,data):
-        self.data=data
-        self.left=None
-        self.right=None
 
-    def insert(self,data):
-        if(self.data):
-            if(data<self.data):
-                if(self.left==None):
-                    self.left=node(data)
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
                 else:
                     self.left.insert(data)
-            if (data > self.data):
-                if (self.right == None):
-                    self.right=node(data)
+            if data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
                 else:
                     self.right.insert(data)
         else:
-            self.data=data
+            self.data = data
 
-    def findVal(self,val):
-        if (val<self.data):
-            if(self.left==None):
-                print("value not found",val)
+    def findval(self, val):
+        if val < self.data:
+            if self.left is None:
+                print("value not found", val)
             else:
-                self.left.findVal(val)
-        elif(val>self.data):
-            if(self.right==None):
-                print("value not found",val)
+                self.left.findval(val)
+        elif val > self.data:
+            if self.right is None:
+                print("value not found", val)
             else:
-                self.right.findVal(val)
+                self.right.findval(val)
         else:
             print("value found")
 
-    def inOrder(self,root):
+    def inOrder(self, root):
         if root:
             self.inOrder(root.left)
             print(root.data, end=" ")
             self.inOrder(root.right)
 
-    def preOrder(self,root):
+    def preOrder(self, root):
         if root:
-            print(root.data,end=" ")
+            print(root.data, end=" ")
             self.preOrder(root.left)
             self.preOrder(root.right)
 
-    def postOrder(self,root):
+    def postOrder(self, root):
         if root:
             self.preOrder(root.right)
             self.preOrder(root.left)
-            print(root.data,end=" ")
+            print(root.data, end=" ")
 
     def printTree(self):
-        if(self.left):
+        if self.left:
             self.left.printTree()
         print(self.data),
-        if(self.right):
+        if self.right:
             self.right.printTree()
 
     def levelOrder(self):
-        l=queue.Queue(maxsize=500)
-        if(self!=None):
+        l = queue.Queue(maxsize=500)
+        if self is not None:
             l.put(self)
-            while(l.empty()==False):
-                k=l.get()
-                print(k.data,end=" ")
-                if(k.left):
+            while not l.empty():
+                k = l.get()
+                print(k.data, end=" ")
+                if k.left:
                     l.put(k.left)
-                if(k.right):
+                if k.right:
                     l.put(k.right)
 
-if __name__=='__main__':
-    root=node(23)
+    def height(self, root):
+        if root is not None:
+
+            leftHeight = self.height(root.left)
+            rightHeight = self.height(root.right)
+            return max(leftHeight, rightHeight) + 1
+        else:
+            return -1
+
+    def getlevel(self, root, data, level):
+        if root is None:
+            return 0
+        if root.data == data:
+            return level
+        downlevel = self.getlevel(root.left, data, level + 1)
+        if downlevel != 0:
+            return downlevel
+        downlevel = self.getlevel(root.right, data, level + 1)
+        return downlevel
+
+
+if __name__ == '__main__':
+    root = Node(23)
     root.insert(60)
     root.insert(5)
     root.insert(15)
     root.insert(28)
-    root.findVal(23)
-    root.findVal(21)
+    root.findval(23)
+    root.findval(21)
     root.printTree()
     root.inOrder(root)
     print()
@@ -88,4 +111,7 @@ if __name__=='__main__':
     root.postOrder(root)
     print()
     root.levelOrder()
-
+    print()
+    height = root.height(root)
+    print("height = ", height)
+    print("level =",root.getlevel(root, 28, 1))
